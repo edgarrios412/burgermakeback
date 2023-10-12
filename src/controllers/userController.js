bcd43acaa9bc58cc7bad7922769aa1ca8055cb0c
unknown <edgarrios412@gmail.com>
2023-10-12 12:00:11 -0500

    push

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,8 +16,8 @@ module.exports = {
         // console.log(user.lenght)
         if(user.length){
             const token = jwt.sign({id:user[0].id, email:user[0].email, role:user[0].role, name:user[0].name,lastname:user[0].lastname, image:user[0].image}, "natalie")
-            console.log(user)
-            console.log(token)
+            // console.log(user)
+            // console.log(token)
             if(user.length) return {status:true, token:token, user:{id:user[0].id, email:user[0].email, role:user[0].role, name:user[0].name,lastname:user[0].lastname, image:user[0].image}}
         }
         return {status:false}
