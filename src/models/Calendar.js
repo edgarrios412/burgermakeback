5363bb42e2f7430347872dced93ec5cef75a310a
unknown <edgarrios412@gmail.com>
2023-10-17 18:26:16 -0500

    push

--- src/models/Calendar.js
+++ src/models/Calendar.js
@@ -6,6 +6,10 @@ module.exports = (sequelize) => {
       primaryKey:true,
       autoIncrement:true,
     },
+    procedimiento: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
     title: {
       type: DataTypes.STRING,
       allowNull: true,
